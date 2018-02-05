28bc1441a1a86ad4cd455dcfe52822c10412c6ed
Max Wolfs <max.wolfs@gmx.de>
2018-02-05 23:15:02 +0100

    Update

--- code/Lox_0.1.ino/RotaryEncoder-master/examples/SimplePollRotator/SimplePollRotator.ino
+++ code/Lox_0.1.ino/RotaryEncoder-master/examples/SimplePollRotator/SimplePollRotator.ino
@@ -1,5 +1,5 @@
 // -----
-// InterruptRotator.ino - Example for the RotaryEncoder library.
+// SimplePollRotator.ino - Example for the RotaryEncoder library.
 // This class is implemented for use with the Arduino environment.
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
@@ -24,38 +24,20 @@ void setup()
 {
   Serial.begin(57600);
   Serial.println("SimplePollRotator example for the RotaryEncoder library.");
-
-  // You may have to modify the next 2 lines if using other pins than A2 and A3
-  PCICR |= (1 << PCIE1);    // This enables Pin Change Interrupt 1 that covers the Analog input pins or Port C.
-  PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);  // This enables the interrupt for pin 2 and 3 of Port C.
 } // setup()
-
-
-
-// The Interrupt Service Routine for Pin Change Interrupt 1
-// This routine will only be called on any signal change on A2 and A3: exactly where we need to check.
-ISR(PCINT1_vect) {
-  encoder.tick(); // just call tick() to check the state.
-}
 
 
 // Read the current position of the encoder and print out when changed.
 void loop()
 {
   static int pos = 0;
+  encoder.tick();
 
   int newPos = encoder.getPosition();
   if (pos != newPos) {
     Serial.print(newPos);
     Serial.println();
     pos = newPos;
-
-    // Just to show, that long lasting procedures don't break the rotary encoder:
-    // When newPos is 66 the ouput will freeze, but the turned positions will be recognized even when not polled.
-    // The interrupt still works.
-    // The output is correct 6.6 seconds later.
-    if (newPos == 66)
-      delay(6600);
   } // if
 } // loop ()
 
